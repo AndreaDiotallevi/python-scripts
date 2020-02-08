c4e9f59785d5543e92ce854afc1c052ac2444097
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2020-02-08 09:02:17 +0000

    feat: create JSON

--- scraper4.py
+++ scraper4.py
@@ -4,7 +4,6 @@ import requests
 import pandas as pd
 import csv, json
 
-csvFilePath = "movies.csv"
 jsonFilePath = 'movies.json'
 
 PATH = os.path.join("/Users/andreadiotallevi/Code/AndreaDiotallevi/web-scraping")
@@ -16,15 +15,22 @@ soup = bs4.BeautifulSoup(page.content, 'lxml')
 div = soup.find(name='div', attrs={'id':'sidebar-right-1'})
 
 
-data = {}
-with open(csvFilePath) as csvFile:
-  for li in soup.findAll('li'):
-    page = requests.get(li.find('a').get('href'))
-    soup = bs4.BeautifulSoup(page.content, 'lxml')
-    div = soup.find(name='div', attrs={'id':'main'})
-    for b in soup.findAll('b'):
-      if not b.find('a'):
-        print(b.text)
+data = []
+for li in soup.findAll('li'):
+  page = requests.get(li.find('a').get('href'))
+  soup = bs4.BeautifulSoup(page.content, 'lxml')
+  div = soup.find(name='div', attrs={'id':'main'})
+  for i, b in enumerate(soup.findAll('b')):
+    if i == 0:
+      country = b.text.split("FROM ")[-1]
+      movies = []
+    elif (not b.find('a')) and (b.text.strip()):
+      movies.append(b.text.encode('utf-8'))
+      # print(b.text)
+  print({country:movies})
+  # data.append({ country: movies })
+
+
 
 # with open(jsonFilePath, "w") as jsonFile:
 #   jsonFile.write(json.dumps(data, indent=4))
